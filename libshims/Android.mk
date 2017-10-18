rk2810/android_msm8996-common

msm8996-common: Add rild socket shim

libshims/Android.mk
@@ -33,3 +33,12 @@ LOCAL_MODULE := libshims_get_process_name
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := rild_socket.c
+
+LOCAL_MODULE := rild_socket
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)

ACollect/diordna_ecived_imoaix_6998msm

Change-Id: Ia5aa2cf867678045215625f6297708db6f49fdaa

libshims/Android.mk
@@ -24,3 +24,12 @@ LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := rild_socket.c
+
+LOCAL_MODULE := libshims_rild_socket
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)